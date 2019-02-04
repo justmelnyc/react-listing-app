473e62d15de9ba0c80a5a144da99854c92d389e6
vilvaathiban-sixt <vilva.athiban@sixt.com>
2019-02-04 23:29:54 +0530

    Added Lint Staged

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,7 @@ import logo from './logo.svg';
 
 class App extends Component {
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">test</div>;
   }
 }
 
